9e5bf1d6f28c7bcb8fc49705076b47fb96e03798
sanjeev <sanjeev.itprofessioal@gmail.com>
2026-01-26 23:15:52 +0530

    code is updated in App.java

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello DevOps World!");
-		System.out.println("Devops is wonderful!")
+        System.out.println("Devops is wonderful!");
+		
     }
 }
